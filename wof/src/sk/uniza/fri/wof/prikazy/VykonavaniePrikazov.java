5b6bee62fae49fee37393eb83ebe434df1d18eec
Jan Janech <jan.janech@kst.uniza.sk>
2021-04-23 11:22:11 +0200

    09b - oprava checkstyle

--- wof/src/sk/uniza/fri/wof/prikazy/VykonavaniePrikazov.java
+++ wof/src/sk/uniza/fri/wof/prikazy/VykonavaniePrikazov.java
@@ -11,7 +11,11 @@ import sk.uniza.fri.wof.svet.npc.Npc;
 import sk.uniza.fri.wof.svet.npc.NpcOslovitelne;
 import sk.uniza.fri.wof.svet.npc.Obchodnik;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Trieda NazvyPrikazov udrzuje zoznam nazvov platnych prikazov hry. 
