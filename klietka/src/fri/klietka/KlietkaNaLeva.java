294885757fa0181a46257740070dbe018e63377a
Jan Janech <jan.janech@kst.uniza.sk>
2021-04-30 11:31:17 +0200

    10b - funkcna klietka

--- klietka/src/fri/klietka/KlietkaNaLeva.java
+++ klietka/src/fri/klietka/KlietkaNaLeva.java
@@ -1,11 +1,21 @@
 package fri.klietka;
 
 public class KlietkaNaLeva {
-    public void vloz(Lev lev) {
+    private Lev lev;
 
+    public KlietkaNaLeva() {
+        this.lev = null;
+    }
+
+    public void vloz(Lev lev) {
+        this.lev = lev;
     }
 
     public void vypis() {
-
+        if (this.lev == null) {
+            System.out.println("Klietka je prazdna");
+        } else {
+            System.out.println("V klietke je lev");
+        }
     }
 }
