b67c4665c549d6d44b37098dc471ecac1ea3311a
Jan Janech <jan.janech@kst.uniza.sk>
2021-05-07 11:55:46 +0200

    11b - projekt pre prvocisla

---
+++ prvocisla/src/fri/prvocisla/Main.java
@@ -0,0 +1,11 @@
+package fri.prvocisla;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Prvocisla p = new Prvocisla(50);
+        for (Integer cislo : p) {
+            System.out.println(cislo);
+        }
+    }
+}
