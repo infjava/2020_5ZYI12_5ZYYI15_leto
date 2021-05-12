0584854066f187bd8328be1f6740b461827a7534
Jan Janech <jan.janech@kst.uniza.sk>
2021-05-12 09:09:05 +0200

    12a - pekne okno pomocou kombinacie Grid a Border layoutov

--- vtipnaAplikacia/src/fri/vtip/OknoOtazky.java
+++ vtipnaAplikacia/src/fri/vtip/OknoOtazky.java
@@ -3,7 +3,9 @@ package fri.vtip;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 public class OknoOtazky {
     private final JFrame okno;
@@ -11,11 +13,16 @@ public class OknoOtazky {
     public OknoOtazky() {
         this.okno = new JFrame("Otazka");
 
-        this.okno.setLayout(new BorderLayout());
+        this.okno.setLayout(new BorderLayout(0, 10));
 
         this.okno.add(new JLabel("Chces uspesne skoncit predmet Informatika 2?"), BorderLayout.NORTH);
-        this.okno.add(new JButton("Ano"), BorderLayout.WEST);
-        this.okno.add(new JButton("Nie"), BorderLayout.EAST);
+
+        JPanel tlacitka = new JPanel();
+        tlacitka.setLayout(new GridLayout(1, 2, 10, 0));
+        tlacitka.add(new JButton("Ano"));
+        tlacitka.add(new JButton("Nie"));
+
+        this.okno.add(tlacitka, BorderLayout.CENTER);
 
         this.okno.pack();
     }
