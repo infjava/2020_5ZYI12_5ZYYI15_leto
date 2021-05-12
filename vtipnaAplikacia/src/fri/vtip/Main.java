d4f899cbd768801116cd65893b99f75e5179d89f
Jan Janech <jan.janech@kst.uniza.sk>
2021-05-12 08:47:10 +0200

    12a - zadanie pre vtipnu aplikaciu - zatial pomocou JOptionPane

--- vtipnaAplikacia/src/fri/vtip/Main.java
+++ vtipnaAplikacia/src/fri/vtip/Main.java
@@ -1,9 +1,13 @@
 package fri.vtip;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
     public static void main(String[] args) {
-        //TODO Write your code
-        System.out.println("Hello world");
+        int vyber = JOptionPane.showConfirmDialog(null, "Chces uspesne skoncit predmet Informatika 2?");
+        if (vyber == JOptionPane.NO_OPTION) {
+            JOptionPane.showMessageDialog(null, "To myslis vazne?");
+        }
     }
 }
