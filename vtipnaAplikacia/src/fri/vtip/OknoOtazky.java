68107922f3efab93d81ba9b1016ccd6b3005fd29
Jan Janech <jan.janech@kst.uniza.sk>
2021-05-12 09:03:05 +0200

    12a - okno obsahuje vsetky komponenty

--- vtipnaAplikacia/src/fri/vtip/OknoOtazky.java
+++ vtipnaAplikacia/src/fri/vtip/OknoOtazky.java
@@ -1,12 +1,21 @@
 package fri.vtip;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
 
 public class OknoOtazky {
     private final JFrame okno;
 
     public OknoOtazky() {
         this.okno = new JFrame("Otazka");
+
+        this.okno.setLayout(new BorderLayout());
+
+        this.okno.add(new JLabel("Chces uspesne skoncit predmet Informatika 2?"), BorderLayout.NORTH);
+        this.okno.add(new JButton("Ano"), BorderLayout.WEST);
+        this.okno.add(new JButton("Nie"), BorderLayout.EAST);
 
         this.okno.pack();
     }
