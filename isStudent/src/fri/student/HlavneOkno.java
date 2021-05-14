51cb0a5f1fbec0a7efda05ac6bc1374c348a41c8
Jan Janech <jan.janech@kst.uniza.sk>
2021-05-14 11:48:45 +0200

    12b - hlavne okno hotove

--- isStudent/src/fri/student/HlavneOkno.java
+++ isStudent/src/fri/student/HlavneOkno.java
@@ -1,17 +1,28 @@
 package fri.student;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
+import java.awt.Dimension;
 
 public class HlavneOkno {
     private JPanel isStudent;
+    private JList studenti;
+    private JTextField meno;
+    private JTextField priezvisko;
+    private JButton novy;
+    private JButton editovat;
+    private JButton vymazat;
 
     public void zobraz() {
         JFrame okno = new JFrame("IS Student");
         okno.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         okno.add(this.isStudent);
         okno.pack();
+        okno.setMinimumSize(new Dimension(350, 250));
         okno.setVisible(true);
     }
 }
